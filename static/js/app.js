a60490033910525a07abf437464ae0022ba34885
Jewell Foster <60074350+thinice209@users.noreply.github.com>
2020-06-12 18:38:00 -0400

    Date works
    Gets all inputs. Need to figure out how to make it run if not all inputs are entered.

--- static/js/app.js
+++ static/js/app.js
@@ -19,10 +19,43 @@ var button = d3.select("#button");
 
 var form = d3.select("#form")
 
-button.on("click", function() {
+button.on("click", runEnter)
+form.on("submit",runEnter);
 
+function runEnter() {
     d3.event.preventDefault();
-    console.log("You clicked a button");
+    tbody= tbody;
+    var inputDateTime = d3.select("#inputDateTime");
+    var dateValue = inputDateTime.property("value");
+    var inputState = d3.select("#inputState");
+    var stateValue = inputState.property("value");
+    var inputCountry = d3.select("#inputCountry");
+    var countryValue = inputCountry.property("value");
+    var inputShape = d3.select("#inputShape");
+    var shapeValue = (inputShape.property("value")).toLowerCase();
+    var inputCity = d3.select("#inputCity");
+    var cityValue = (inputCity.property("value")).toLowerCase();
 
-
-})
+    console.log(dateValue);
+    console.log(stateValue);
+    console.log(countryValue);
+    console.log(shapeValue);
+    console.log(cityValue);
+   
+    // var filteredData = tableData.filter(sighting => sighting.datetime === dateValue)
+    var filteredData = tableData.filter(sighting => sighting.datetime === dateValue && sighting.state === stateValue && 
+        sighting.country === countryValue && sighting.shape === shapeValue && sighting.city === cityValue);
+    
+    console.log(filteredData);
+    document.getElementById("body").innerHTML="";
+    tableData.push(filteredData.forEach(function (ufoSighting) {
+                var row = tbody.append("tr");
+        
+        Object.values(ufoSighting).forEach(function(value) {
+        
+            var cell= tbody.append("td");
+            cell.text(value);
+        })
+    }));
+      
+}
